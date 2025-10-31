0ca0d0bb89ac1a31534445cd6808e10a396889ea
Nemo <ejongwon7@gmail.com>
2025-11-01 01:44:14 +0900

    Feat: adds answers directory

---
+++ Attack Lab/answers/solve_calmly copy.sh
@@ -0,0 +1,5 @@
+./ctarget -i level_1_solution
+./ctarget -i level_2_solution
+./ctarget -i level_3_solution
+./rtarget -i level_4_solution
+./rtarget -i level_5_solution
---
+++ Attack Lab/answers/solve_calmly.sh
@@ -0,0 +1,5 @@
+./ctarget -q -i level_1_solution
+./ctarget -q -i level_2_solution
+./ctarget -q -i level_3_solution
+./rtarget -q -i level_4_solution
+./rtarget -q -i level_5_solution
