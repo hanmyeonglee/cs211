94357adde44c97f7b50e77b7b9d0294b0e0d9f1f
ejongwon7 <ejongwon7@gmail.com>
2025-11-10 22:21:56 +0900

    Feat: implements read one line function

--- Cache Lab/csim.c
+++ Cache Lab/csim.c
@@ -4,17 +4,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define ASSUMED_LINE_LENGTH 256
+
 void usage() {
     puts(
         "Usage: ./csim [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n"
         "Options:\n"
-        "  -h               Optional help flag that prints usage info\n"
-        "  -v               Optional verbose flag that displays trace info\n"
-        "  -s <num>         Number of set index bits (S = 2^s is the number of sets)\n"
-        "  -E <num>         Associativity (number of lines per set)\n"
-        "  -b <num>          Number of block bits (B = 2^b is the block size)\n"
-        "  -t <filename>    Name of the `valgrind` trace to replay\n"
+        "  -h            :  Optional help flag that prints usage info\n"
+        "  -v            :  Optional verbose flag that displays trace info\n"
+        "  -s <num>      :  Number of set index bits (S = 2^s is the number of sets)\n"
+        "  -E <num>      :  Associativity (number of lines per set)\n"
+        "  -b <num>      :  Number of block bits (B = 2^b is the block size)\n"
+        "  -t <filename> :  Name of the `valgrind` trace to replay\n"
     );
+    exit(1);
+}
+
+char *get_next_line(FILE *fp, bool keepends) {
+    char *line = NULL;
+    char buffer[ASSUMED_LINE_LENGTH];
+    size_t size = 0, len = 0;
+
+    while(
+        fgets(buffer, ASSUMED_LINE_LENGTH, fp) != NULL
+    ) {
+        size_t buffer_len = strlen(buffer);
+        if (len + buffer_len + 1 > size) {
+            size += ASSUMED_LINE_LENGTH;
+            char *temp = realloc(line, size);
+            if (temp == NULL) {
+                free(line);
+                return NULL;
+            }
+
+            line = temp;
+        }
+
+        strcpy(line + len, buffer);
+        len += buffer_len;
+        if (buffer[buffer_len - 1] == '\n') {
+            if (!keepends) {
+                line[len - 1] = '\0';
+            } else {
+                line[len] = '\0';
+            }
+            break;
+        }
+    }
+
+    return line;
+}
+
+char *get_next_line(FILE *fp) {
+    return get_next_line(fp, false);
 }
 
 int main(int argc, char *argv[])
@@ -33,7 +75,6 @@ int main(int argc, char *argv[])
         switch (opt) {
             case 'h':
                 usage();
-                break;
             case 'v':
                 verbose = true;
                 break;
@@ -52,13 +93,12 @@ int main(int argc, char *argv[])
             default:
                 printf("%s is not a valid option\n", argv[optind - 1]);
                 usage();
-                exit(1);
         }
     }
 
-    FILE *file = fopen(trace_file, "r");
+    FILE *fp = fopen(trace_file, "r");
     // check required arguments
-    if (s == -1 || E == -1 || b == -1 || trace_file == NULL || file == NULL) {
+    if (s == -1 || E == -1 || b == -1 || trace_file == NULL || fp == NULL) {
         printf("Missing required command line argument\n");
         usage();
         exit(1);
@@ -67,6 +107,8 @@ int main(int argc, char *argv[])
     int S = 1 << s; // number of sets
     int B = 1 << b; // block size in bytes
     int hits = 0, misses = 0, evictions = 0;
+
+    
 
     printSummary(hits, misses, evictions);
     return 0;
