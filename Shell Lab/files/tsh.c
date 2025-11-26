07f416010562a2b3148546e08b1ddb4360f071c4
ejongwon7 <ejongwon7@gmail.com>
2025-11-27 01:45:20 +0900

    Feat: adds SIO

--- Shell Lab/files/tsh.c
+++ Shell Lab/files/tsh.c
@@ -357,6 +357,101 @@ void waitfg(pid_t pid)
     return;
 }
 
+/*************************************************************
+ * The Sio (Signal-safe I/O) package - simple reentrant output
+ * functions that are safe for signal handlers.
+ *************************************************************/
+
+/* Private sio functions */
+
+/* $begin sioprivate */
+/* sio_reverse - Reverse a string (from K&R) */
+static void sio_reverse(char s[])
+{
+    int c, i, j;
+
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/* sio_ltoa - Convert long to base b string (from K&R) */
+static void sio_ltoa(long v, char s[], int b) 
+{
+    int c, i = 0;
+    int neg = v < 0;
+
+    if (neg)
+	v = -v;
+
+    do {  
+        s[i++] = ((c = (v % b)) < 10)  ?  c + '0' : c - 10 + 'a';
+    } while ((v /= b) > 0);
+
+    if (neg)
+	s[i++] = '-';
+
+    s[i] = '\0';
+    sio_reverse(s);
+}
+
+/* sio_strlen - Return length of string (from K&R) */
+static size_t sio_strlen(char s[])
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+        ++i;
+    return i;
+}
+/* $end sioprivate */
+
+/* Public Sio functions */
+/* $begin siopublic */
+
+ssize_t sio_puts(char s[]) /* Put string */
+{
+    return write(STDOUT_FILENO, s, sio_strlen(s)); //line:csapp:siostrlen
+}
+
+ssize_t sio_putl(long v) /* Put long */
+{
+    char s[128];
+    
+    sio_ltoa(v, s, 10); /* Based on K&R itoa() */  //line:csapp:sioltoa
+    return sio_puts(s);
+}
+
+void sio_error(char s[]) /* Put error message and exit */
+{
+    sio_puts(s);
+    _exit(1);                                      //line:csapp:sioexit
+}
+/* $end siopublic */
+
+/*******************************
+ * Wrappers for the SIO routines
+ ******************************/
+ssize_t Sio_putl(long v)
+{
+    ssize_t n;
+  
+    if ((n = sio_putl(v)) < 0)
+	sio_error("Sio_putl error");
+    return n;
+}
+
+ssize_t Sio_puts(char s[])
+{
+    ssize_t n;
+  
+    if ((n = sio_puts(s)) < 0)
+	sio_error("Sio_puts error");
+    return n;
+}
+
 /*****************
  * Signal handlers
  *****************/
@@ -383,7 +478,13 @@ void sigchld_handler(int sig)
         }
         else if (WIFSIGNALED(wstatus)) {
             int jid = pid2jid(child_pid);
-            printf("Job [%d] (%d) terminated by signal %d\n", jid, child_pid, WTERMSIG(wstatus));
+            Sio_puts("Job [");
+            Sio_putl(jid);
+            Sio_puts("] (");
+            Sio_putl(child_pid);
+            Sio_puts(") terminated by signal ");
+            Sio_putl(WTERMSIG(wstatus));
+            Sio_puts("\n");
             deletejob(jobs, child_pid);
         }
         else if (WIFSTOPPED(wstatus)) {
@@ -391,7 +492,13 @@ void sigchld_handler(int sig)
             struct job_t *job = getjobpid(jobs, child_pid);
             if (job != NULL) {
                 job->state = ST;
-                printf("Job [%d] (%d) stopped by signal %d\n", jid, child_pid, WSTOPSIG(wstatus));
+                Sio_puts("Job [");
+                Sio_putl(jid);
+                Sio_puts("] (");
+                Sio_putl(child_pid);
+                Sio_puts(") stopped by signal ");
+                Sio_putl(WSTOPSIG(wstatus));
+                Sio_puts("\n");
             }
         }
     }
